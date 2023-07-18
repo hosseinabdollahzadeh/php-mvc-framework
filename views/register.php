801bbf6c8e44fe28df881a813fcdf6d61605ae2a
hossein <abdollahzadeh.hossein@gmail.com>
2023-07-18 12:16:58 +0330

    Models, Forms, Validations

--- views/register.php
+++ views/register.php
@@ -1,32 +1,17 @@
 <h1>Create an account</h1>
-
-<form action="" method="post">
+<?php $form = \app\core\form\Form::begin('', "post") ?>
     <div class="row">
         <div class="col">
-            <div class="form-group">
-                    <label>FirstName</label>
-                    <input name="firstName" type="text" class="form-control">
-                </div>
+            <?= $form->field($model, 'firstName') ?>
         </div>
         <div class="col">
-            <div class="form-group">
-                <label>LastName</label>
-                <input name="lastName" type="text" class="form-control">
-            </div>
+            <?= $form->field($model, 'lastName') ?>
         </div>
     </div>
 
-    <div class="form-group">
-        <label>Email</label>
-        <input name="email" type="email" class="form-control">
-    </div>
-    <div class="form-group">
-        <label>Password</label>
-        <input name="password" type="password" class="form-control">
-    </div>
-    <div class="form-group">
-        <label>Confirm Password</label>
-        <input name="confirmPassword" type="password" class="form-control">
-    </div>
+    <?= $form->field($model, 'email') ?>
+    <?= $form->field($model, 'password')->passwordField() ?>
+    <?= $form->field($model, 'confirmPassword')->passwordField() ?>
+
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php \app\core\form\Form::end() ?>
