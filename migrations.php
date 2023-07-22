b1b2f61d623b51ae886e46b4640d751a94593935
hossein <abdollahzadeh.hossein@gmail.com>
2023-07-22 13:14:02 +0330

    Add Dockerfile and docker-compose.yml

--- migrations.php
+++ migrations.php
@@ -3,7 +3,7 @@ use app\controllers\AuthController;
 use app\controllers\SiteController;
 use abiz\phpmvc\Application;
 
-require_once __DIR__ . './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
